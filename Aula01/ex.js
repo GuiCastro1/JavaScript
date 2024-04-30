fbc549e9141f895b6c14ae446409570a19c07aae
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2024-04-30 17:11:00 -0300

    Salvando Primeira aula de JavaScript

---
+++ Aula01/ex.js
@@ -0,0 +1,9 @@
+
+let nota1 = Number(prompt("Digite a 1° nota"));
+let nota2 = Number(prompt("Digite a 2° nota"));
+let nota3 = Number(prompt("Digite a 3° nota"));
+let nota4 = Number(prompt("Digite a 4° nota"));
+
+let soma = (nota1 + nota2 + nota3 + nota4) / 4;
+
+alert(` sua media é ${soma}`)
