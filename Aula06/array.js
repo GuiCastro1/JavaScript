f593d1df5ea68bf4bda42497f31f8d93afd214c9
Guilherme Castro <gui.castro.0001.1426@gmail.com>
2024-05-13 14:02:39 -0300

    Teste de push

---
+++ Aula06/array.js
@@ -0,0 +1,8 @@
+let tcc = ['gui', 'mari', 'davi', 'fabiano', 'ana', 'joão']
+
+const tcc1 = new Array ('banana', 'maça', 'uva', 'pera', 'melão', 'jambo', 'Tamarindo') 
+
+console.log(tcc);
+console.log(tcc[0]);
+console.log(tcc1);
+console.log(tcc1[6]);
